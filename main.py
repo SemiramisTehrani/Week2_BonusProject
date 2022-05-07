88ecf51998759355bdd934eabe37de7a8c8ee08a
unknown <semtehrani@yahoo.com>
2022-05-07 15:07:49 -0700

    Finished dictionary part

---
+++ main.py
@@ -0,0 +1,47 @@
+# Bonus Project Week 2 
+# You are Hercules, the greatest of the Greek Heroes! You have been tasked by King Eurystheus to 
+# slay the vicious Nemean Lion, 
+# defeat the impossible nine-headed Lernaean Hydra, 
+# and capture the guard dog of the underworld—Cerberus.
+
+
+print("I am  Hercules, the greatest of the Greek Heroes! I have been tasked by King Eurystheus to slay ...the vicious Nemean Lion, defeat the impossible nine-headed Lernaean Hydra, and capture the guard dog of the underworld—Cerberus.")
+
+# my version vefore Friday 101 meeting
+# Hero = "Hercules"
+# Enemy_List = ['Nemean_Lion' , 'Lernaean_Hydra' , 'Cerberus']
+# Enemy_Attack_Names_List = ['vicious','Nine_Headed', 'Dog_gaurd_of_under_water']
+
+
+# def Attack(Hercules_health, enemy_health) :
+#     if Hercules_health or  enemy_health == False :  
+#         outcome = print(" Hercules's or Enemy's health reached zero : fight is over ")
+#     else:
+#         outcome = print(" Hercules's or Enemy's health is Ok : fighting continues ")
+#     return outcome
+
+
+# print("Enter Hercules's health level  1 (strong) or 0 (weak) : ")
+
+# Hercules_health = int(input())
+
+# print("Enter Enemy's health level  1 (strong) or 0 (weak) : ")
+# Enemy_health = int(input())
+
+# Attack_health_result = Attack(Hercules_health, Enemy_health)
+
+
+
+# my version after Friday 101 meeting
+
+Hercues_dic = {
+    "attack_power": 200, 
+    "health_level" : 300
+    }
+
+Oponent_dic = {
+    "attack_power": 100, 
+    "health_level" : 150
+    }
+
+
